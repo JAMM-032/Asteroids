25e4837952bebba9439e4bddb8627a42e2529174
Dmitrij Zlobicki <dmitrij.zlobicki@gmail.com>
2025-03-21 09:10:17 +0000

    Changed multiplier bar to display TTL.

--- src/Score.java
+++ src/Score.java
@@ -1,5 +1,8 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+import java.awt.*;
 
 /**
  * Score class manages the player's score and multiplier system.
@@ -10,6 +13,11 @@ public class Score {
     private static final int BAR_WIDTH = 60;
     private static final int BAR_HEIGHT = 8;
     private static final int TEXT_OFFSET = 20;
+
+    private static final int CANVAS_WIDTH = 600;
+    private static final int CANVAS_HEIGHT = 600;
+    private static final int X_OFFSET = 50;
+    private static final int Y_OFFSET = 10;
 
     private int multiplierTTL; // Time after which multiplier resets
     private int score;
@@ -25,8 +33,9 @@ public class Score {
         initializeMultiplierBar();
     }
 
+
     /**
-     * Initializes the multiplier bar and outline.
+     * Initializes the multiplier time bar and outline.
      */
     private void initializeMultiplierBar() {
         // Outline remains constant
@@ -34,15 +43,17 @@ public class Score {
                 new double[]{BAR_WIDTH / 2, BAR_WIDTH / 2, -BAR_WIDTH / 2, -BAR_WIDTH / 2},
                 new double[]{BAR_HEIGHT / 2, -BAR_HEIGHT / 2, -BAR_HEIGHT / 2, BAR_HEIGHT / 2}
         );
-        multiplierOutline.translate(new Vector2(550, 10 + BAR_HEIGHT));
+        multiplierOutline.translate(new Vector2(CANVAS_WIDTH-X_OFFSET, Y_OFFSET + BAR_HEIGHT));
 
         // Multiplier bar (changes dynamically)
         multiplierBar = new Shape(
                 new double[]{BAR_WIDTH / 2, BAR_WIDTH / 2, 0, 0},
                 new double[]{BAR_HEIGHT / 2, -BAR_HEIGHT / 2, -BAR_HEIGHT / 2, BAR_HEIGHT / 2}
         );
-        multiplierBar.translate(new Vector2(550 - BAR_WIDTH / 2, 10 + BAR_HEIGHT));
+        multiplierBar.translate(new Vector2(CANVAS_WIDTH-X_OFFSET - BAR_WIDTH / 2, Y_OFFSET + BAR_HEIGHT));
     }
+
+
 
     /**
      * Increases the score with the current multiplier.
@@ -68,7 +79,7 @@ public class Score {
      */
     public void resetMultiplier() {
         multiplier = 1.0;
-        multiplierTTL = INITIAL_MULTIPLIER_TTL;
+        multiplierTTL = 0;
     }
 
     /**
@@ -76,21 +87,24 @@ public class Score {
      * If TTL expires, resets the multiplier.
      */
     public void update() {
-        if (--multiplierTTL <= 0) {
-            resetMultiplier();
+        if (multiplierTTL > 0) {
+            --multiplierTTL;
+            if (multiplierTTL <= 0) {
+                resetMultiplier();
+            }
         }
     }
 
     /**
      * Updates the multiplier bar's size based on the multiplier value.
      */
-    private void updateBar() {
-        double widthScale = (multiplier - 1) / (MAX_MULTIPLIER - 1);
+    private void updateMultiplierBar() {
+        double widthScale = (double) multiplierTTL / (double) INITIAL_MULTIPLIER_TTL;
         double[] xPoints = {BAR_WIDTH * widthScale, BAR_WIDTH * widthScale, 0, 0};
         double[] yPoints = {BAR_HEIGHT / 2, -BAR_HEIGHT / 2, -BAR_HEIGHT / 2, BAR_HEIGHT / 2};
 
         multiplierBar = new Shape(xPoints, yPoints);
-        multiplierBar.translate(new Vector2(550 - BAR_WIDTH / 2, 10 + BAR_HEIGHT));
+        multiplierBar.translate(new Vector2(CANVAS_WIDTH-X_OFFSET - BAR_WIDTH / 2, Y_OFFSET + BAR_HEIGHT));
     }
 
     /**
@@ -99,35 +113,29 @@ public class Score {
      * @param gc The GraphicsContext to draw on
      */
     public void draw(GraphicsContext gc) {
-        updateBar();
+        updateMultiplierBar();
 
         // Draw multiplier bar & outline
         multiplierOutline.drawStroke(gc, Color.WHITE);
         multiplierBar.drawFill(gc, Color.WHITE);
 
-        // Draw multiplier numbers below the bar
-        drawMultiplierLabels(gc);
+        // Draw multiplier value below the bar
+        drawMultiplierLabel(gc);
 
         // Draw Score
-        gc.setFont(new javafx.scene.text.Font(15));
-        gc.fillText("Score: " + score, 35, 20);
+        gc.setFont(Font.font("Courier New", 15));
+        gc.fillText(String.valueOf(score), 35, 20);
     }
 
     /**
-     * Draws numbers below the multiplier bar (1, MAX_MULTIPLIER, and current value).
+     * Draws number of time after which multiplier resets
      */
-    private void drawMultiplierLabels(GraphicsContext gc) {
-        double leftX = 550 - BAR_WIDTH / 2;   // Left bar edge
-        double rightX = 550 + BAR_WIDTH / 2;  // Right bar edge
-        double centerX = 550;                 // Bar center
-        double textY = 10 + BAR_HEIGHT + TEXT_OFFSET; // Position below the bar
+    private void drawMultiplierLabel(GraphicsContext gc) {
+        double textY = Y_OFFSET + BAR_HEIGHT + TEXT_OFFSET; // Position below the bar
 
         gc.setFill(Color.WHITE);
-        gc.setFont(new javafx.scene.text.Font(10));
-
-        gc.fillText("1", leftX, textY);
-        gc.fillText(String.valueOf(MAX_MULTIPLIER), rightX, textY);
-        gc.fillText("x" + String.format("%.2f", multiplier), centerX, textY);
+        gc.setFont(Font.font("Courier New", 10));
+        gc.fillText(String.format("%.2f", multiplier), CANVAS_WIDTH-X_OFFSET, textY);
     }
 
     public int getScore() {
